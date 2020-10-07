8015ac1941a838ef6cb9e2e7b53026e84fc2a8b6
Sachin Saini <45393031+eddygrant000@users.noreply.github.com>
2020-10-07 22:15:29 +0530

    Create setup_jenkins.sh

---
+++ setup_jenkins.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install openjdk-8-jdk -y
+sudo apt update
+sudo apt install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+java -version
